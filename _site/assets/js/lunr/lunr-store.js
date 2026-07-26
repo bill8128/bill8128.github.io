24f3f0e6126982f9473e089559958b185f9359b7
bill8128 <bill.swalwell@blueyonder.co.uk>
2026-07-26 17:51:29 +0100

    Add paintings

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,38 @@
 var store = [{
+        "title": "chaffinch",
+        "excerpt":"Write a description of the painting here.  ","categories": [],
+        "tags": [],
+        "url": "/paintings/chaffinch/",
+        "teaser": null
+      },{
+        "title": "cow-parsley",
+        "excerpt":"Write a description of the painting here.  ","categories": [],
+        "tags": [],
+        "url": "/paintings/cow-parsley/",
+        "teaser": null
+      },{
+        "title": "goldcrest",
+        "excerpt":"Write a description of the painting here.  ","categories": [],
+        "tags": [],
+        "url": "/paintings/goldcrest/",
+        "teaser": null
+      },{
         "title": "Humming Bird",
         "excerpt":"Write a description of the painting here.  ","categories": [],
         "tags": [],
         "url": "/paintings/humming-bird/",
+        "teaser": null
+      },{
+        "title": "lakeside",
+        "excerpt":"Write a description of the painting here.  ","categories": [],
+        "tags": [],
+        "url": "/paintings/lakeside/",
+        "teaser": null
+      },{
+        "title": "rydal-hall-gardens",
+        "excerpt":"Write a description of the painting here.  ","categories": [],
+        "tags": [],
+        "url": "/paintings/rydal-hall-gardens/",
         "teaser": null
       },{
         "title": "Vase of Flowers",
